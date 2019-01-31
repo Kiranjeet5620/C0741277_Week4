63e2052430c48206037a81264783d380131fe268
visualstudio <visualstudio@230-PC32>
2019-01-31 11:52:41 -0500

    function created

--- C0741277/js/main.js
+++ C0741277/js/main.js
@@ -1,1 +1,9 @@
-﻿// Your code here!
+﻿
+var date = new Date();
+
+function myFunction() {
+
+
+
+    document.body.innerHTML = "<h1> The date is " + date.getDate() + " / " + (date.getMonth() + 1) + " / " + date.getFullYear() + " </h1>"
+}
